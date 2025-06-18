a249d384eaa4b58a0ad43d3ad0acfa10c601e6ed
Vincenzo <vincenzo.forte12@gmail.com>
2025-06-18 19:10:30 +0200

    xy

---
+++ frontend/Bacheca/src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import HomePage from '../pages/HomePage';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, Outlet } from 'react-router-dom';
+const NotFoundPage = () => <div><h1>404 - Pagina Non Trovata</h1><Link to="/">Torna alla Home</Link></div>;const AppRoutes = () => {
+  return (
+    // Router (BrowserRouter) avvolge tutte le definizioni di rotte.
+    <Router>
+      {/* Routes contiene tutte le definizioni <Route>. */}
+      <Routes>
+        {/* Definizioni delle Rotte Pubbliche: accessibili a tutti gli utenti. */}
+        <Route path="/" element={<HomePage />} /> {/* Rotta per la pagina principale. */}
+        
+      </Routes>
+    </Router>
+  );
+};
+
+// Esporta il componente AppRoutes.
+export default AppRoutes;
